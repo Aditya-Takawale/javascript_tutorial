a7c0daeaf5c8583b8e15b8d74514966babfba54c
Aditya Takawale <155251310+Aditya-Takawale@users.noreply.github.com>
2025-09-16 15:09:21 +0530

    tutorial started

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "adityatakawale@gmail.com"
+var accountPassword = "Aditya@123"
+accountCity = "Pune" 
+let accountState ;
+
+
+// accountId = 2 // not allowed
+accountEmail = "newemail@example.com"
+accountPassword = "newpassword"
+accountCity = "Mumbai"
+
+// console.log(accountId);
+// console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Enabling strict mode for better error checking
+
+// alert("Hello, World!"); // Displaying an alert message but we are using node.js not browser 
+
+console.log(3+3) 
+console.log("Aditya") // Printing to the console
+
+
+let name = "Aditya Takawale" // String datatype
+let age = 21 // Number datatype
+let isLoggedIn = true // Boolean datatype
+
+//  number => 2 to power 53
+// BigInt => larger than 2 to power 53
+//  string => '', "", ``
+//  boolean => true/false
+//  null => standalone value
+//  undefined => value not assigned
+//  object => key value pairs
+//  symbol => unique identifiers
+
+
+
+
+// objects
+
+console.log(typeof undefined); // to check datatype 
