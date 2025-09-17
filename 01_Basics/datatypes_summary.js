eaa40ffb792a95c70b1a84ea87cfd1bdfed09820
Aditya Takawale <155251310+Aditya-Takawale@users.noreply.github.com>
2025-09-17 14:40:09 +0530

    new files

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,26 @@
+// # Primitive  
+
+// 7 categories of data types in JavaScript
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol (ES6)
+// 7. BigInt (ES2020)
+
+// # Non-Primitive (Reference) Data Type
+
+// 1. Object example
+// let person = {
+//     name: "Aditya Takawale",
+
+// 2. Array example
+// let numbers = [1, 2, 3, 4, 5];
+
+// 3. Function example
+// function greet() {
+//     console.log("Hello, World!");
+// }
+
+// # Dynamic Typing
